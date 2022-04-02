ccef9b3a1e41fcfccbc719ef7848a82bf5a02202
geoyee <geoyee@yeah.net>
2022-04-02 13:37:27 +0800

    Fix #36

--- tutorials/train/classification/condensenetv2_b_rs_mul.py
+++ tutorials/train/classification/condensenetv2_b_rs_mul.py
@@ -1,12 +1,7 @@
-import sys
-
-sys.path.append("E:/dataFiles/github/PaddleRS")
-
 import paddlers as pdrs
 from paddlers import transforms as T
 
 # 定义训练和验证时的transforms
-# API说明：https://github.com/PaddlePaddle/paddlers/blob/develop/docs/apis/transforms/transforms.md
 train_transforms = T.Compose([
     T.BandSelecting([5, 10, 15, 20, 25]),  # for tet
     T.Resize(target_size=224),
@@ -23,30 +18,27 @@ eval_transforms = T.Compose([
 ])
 
 # 定义训练和验证所用的数据集
-# API说明：https://github.com/PaddlePaddle/paddlers/blob/develop/docs/apis/datasets.md
 train_dataset = pdrs.datasets.ClasDataset(
-    data_dir='E:/dataFiles/github/PaddleRS/tutorials/train/classification/DataSet',
-    file_list='E:/dataFiles/github/PaddleRS/tutorials/train/classification/DataSet/train_list.txt',
-    label_list='E:/dataFiles/github/PaddleRS/tutorials/train/classification/DataSet/label_list.txt',
+    data_dir='tutorials/train/classification/DataSet',
+    file_list='tutorials/train/classification/DataSet/train_list.txt',
+    label_list='tutorials/train/classification/DataSet/label_list.txt',
     transforms=train_transforms,
     num_workers=0,
     shuffle=True)
 
 eval_dataset = pdrs.datasets.ClasDataset(
-    data_dir='E:/dataFiles/github/PaddleRS/tutorials/train/classification/DataSet',
-    file_list='E:/dataFiles/github/PaddleRS/tutorials/train/classification/DataSet/val_list.txt',
-    label_list='E:/dataFiles/github/PaddleRS/tutorials/train/classification/DataSet/label_list.txt',
+    data_dir='tutorials/train/classification/DataSet',
+    file_list='tutorials/train/classification/DataSet/val_list.txt',
+    label_list='tutorials/train/classification/DataSet/label_list.txt',
     transforms=eval_transforms,
     num_workers=0,
     shuffle=False)
 
-# 初始化模型，并进行训练
-# 可使用VisualDL查看训练指标，参考https://github.com/PaddlePaddle/paddlers/blob/develop/docs/visualdl.md
+# 初始化模型
 num_classes = len(train_dataset.labels)
 model = pdrs.tasks.CondenseNetV2_b(in_channels=5, num_classes=num_classes)
 
-# API说明：https://github.com/PaddlePaddle/paddlers/blob/develop/docs/apis/models/semantic_segmentation.md
-# 各参数介绍与调整说明：https://github.com/PaddlePaddle/paddlers/blob/develop/docs/parameters.md
+# 进行训练
 model.train(
     num_epochs=100,
     pretrain_weights=None,
--- tutorials/train/classification/resnet50_vd_rs.py
+++ tutorials/train/classification/resnet50_vd_rs.py
@@ -1,7 +1,3 @@
-import sys
-
-sys.path.append("E:/dataFiles/github/PaddleRS")
-
 import paddlers as pdrs
 from paddlers import transforms as T
 
@@ -9,7 +5,6 @@ from paddlers import transforms as T
 # https://aistudio.baidu.com/aistudio/datasetdetail/63189
 
 # 定义训练和验证时的transforms
-# API说明：https://github.com/PaddlePaddle/paddlers/blob/develop/docs/apis/transforms/transforms.md
 train_transforms = T.Compose([
     T.Resize(target_size=512),
     T.RandomHorizontalFlip(),
@@ -24,9 +19,8 @@ eval_transforms = T.Compose([
 ])
 
 # 定义训练和验证所用的数据集
-# API说明：https://github.com/PaddlePaddle/paddlers/blob/develop/docs/apis/datasets.md
 train_dataset = pdrs.datasets.ClasDataset(
-    data_dir='E:/dataFiles/github/PaddleRS/tutorials/train/classification/DataSet',
+    data_dir='tutorials/train/classification/DataSet',
     file_list='tutorials/train/classification/DataSet/train_list.txt',
     label_list='tutorials/train/classification/DataSet/label_list.txt',
     transforms=train_transforms,
@@ -34,20 +28,18 @@ train_dataset = pdrs.datasets.ClasDataset(
     shuffle=True)
 
 eval_dataset = pdrs.datasets.ClasDataset(
-    data_dir='E:/dataFiles/github/PaddleRS/tutorials/train/classification/DataSet',
+    data_dir='tutorials/train/classification/DataSet',
     file_list='tutorials/train/classification/DataSet/test_list.txt',
     label_list='tutorials/train/classification/DataSet/label_list.txt',
     transforms=eval_transforms,
     num_workers=0,
     shuffle=False)
 
-# 初始化模型，并进行训练
-# 可使用VisualDL查看训练指标，参考https://github.com/PaddlePaddle/paddlers/blob/develop/docs/visualdl.md
+# 初始化模型
 num_classes = len(train_dataset.labels)
 model = pdrs.tasks.ResNet50_vd(num_classes=num_classes)
 
-# API说明：https://github.com/PaddlePaddle/paddlers/blob/develop/docs/apis/models/semantic_segmentation.md
-# 各参数介绍与调整说明：https://github.com/PaddlePaddle/paddlers/blob/develop/docs/parameters.md
+# 进行训练
 model.train(
     num_epochs=10,
     train_dataset=train_dataset,
