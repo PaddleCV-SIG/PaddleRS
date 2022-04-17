d4ad469ab9fcb0ffa85d12a620cf6f98894dc695
sun222 <30310694+sun222@users.noreply.github.com>
2022-04-17 20:53:48 +0800

    Update prepare.sh

--- test_tipc/prepare.sh
+++ test_tipc/prepare.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-source test_tipc/common_func.sh
+
+source ./STANET_Paddle/test_tipc/common_func.sh
 
 FILENAME=$1
 # MODE be one of ['lite_train_lite_infer']
@@ -19,5 +20,5 @@ trainer_list=$(func_parser_value "${lines[14]}")
 # MODE be one of ['lite_train_lite_infer']
 if [ ${MODE} = "lite_train_lite_infer" ];then
     rm -rf ./test_tipc/data/mini_levir_dataset
-    cd ./test_tipc/data/ && unzip mini_levir_dataset.zip && cd ../../
+    cd ./STANET_Paddle/test_tipc/data/ && unzip mini_levir_dataset.zip && cd ../../
 fi
