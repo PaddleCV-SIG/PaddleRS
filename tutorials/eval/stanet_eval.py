b238c47d0f1f3036cc2e969a055a8a3288ed79eb
sun222 <30310694+sun222@users.noreply.github.com>
2022-04-17 20:49:34 +0800

    Update and rename stanet.py to stanet_eval.py

--- tutorials/eval/stanet_eval.py
+++ tutorials/eval/stanet_eval.py
@@ -2,6 +2,8 @@ import sys
 import paddle
 import os
 import argparse
+#加入环境
+sys.path.append('./STANET_Paddle/')
 import paddlers as pdrs
 from paddlers import transforms as T
 import paddle.nn as nn
@@ -11,27 +13,21 @@ def get_parser():
     parser.add_argument('--data_dir', '-m', type=str, default=None, help='model directory path')
     parser.add_argument('--out_dir', '-s', type=str, default=None, help='path to save inference model')
     parser.add_argument('-lr', type=float, default=0.001, help='lr')
-    parser.add_argument('--decay_step', type=int, default=5000elp='epoch number')  
+    parser.add_argument('--decay_step', type=int, default=5000, help='epoch number')  
     parser.add_argument('--num_epoch', type=int, default=100, help='epoch number')
-    parser.add_argument('--batch_size', type=int, default=8 help='batch size')
-    parser.add_argument('--save_epoch', type=int, default=3 help='save epoch')
-    parser.add_argument('--state_dict_path', type=str, default=None help='where model params')
+    parser.add_argument('--batch_size', type=int, default=8, help='batch size')
+    parser.add_argument('--save_epoch', type=int, default=3, help='save epoch')
+    parser.add_argument('--state_dict_path', type=str, default=None ,help='where model params')
     return parser
 
 if __name__ == "__main__":
     parser = get_parser()
     args = parser.parse_args()
-    E:\game\solo\breanman11\dataset\home\aistudio
     DATA_DIR = args.data_dir
-    
     TRAIN_FILE_LIST_PATH = os.path.join(DATA_DIR,'train.txt')
     EVAL_FILE_LIST_PATH = os.path.join(DATA_DIR,'val.txt')
     TESTLE_LIST_PATH = os.path.join(DATA_DIR,'test.txt')
-    LABEL_LIST_PATH = os.path.join(DATA_DIR,'label.txt
-                                   
-                                   
-    
-    
+    LABEL_LIST_PATH = os.path.join(DATA_DIR,'labels.txt')
     EXP_DIR = args.out_dir
     LR = args.lr
     DECAY_STEP = args.decay_step
@@ -41,13 +37,11 @@ if __name__ == "__main__":
     #训练阶段 batch size
     TRAIN_BATCH_SIZE = args.batch_size
     state_dict_path= args.state_dict_path
-   
     eval_transforms = T.Compose([
         T.Resize(target_size=256),
         T.Normalize(
           mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
     ])
-
     eval_dataset = pdrs.datasets.CDDataset(
         data_dir=DATA_DIR+'/val',
         file_list=EVAL_FILE_LIST_PATH,
@@ -56,13 +50,10 @@ if __name__ == "__main__":
         num_workers=2,
         binarize_labels=True,
     shuffle=False)
-
     # 初始化模型，并进行训练
     # 可使用VisualDL查看训练指标，参考https://github.com/PaddlePaddle/paddlers/blob/develop/docs/visualdl.md
     num_classes = len(eval_dataset.labels)
     model = pdrs.tasks.STANet( in_channels=3, num_classes=num_classes, att_type='PAM', ds_factor=1)     
-                                   
-   
     model.net_initialize(pretrain_weights = state_dict_path)
     eval_metrics = model.evaluate(eval_dataset)
     print(str(eval_metrics))
